c857a684b71c50f4c21e1baf89eeab39d8a4613b
jacklinsir <jacklinsir@sina.com>
2020-01-28 19:49:21 +0800

    Eureka伪集群模块

---
+++ cloud-eureka-7002/src/main/java/com/jacklinsir/cloud/CloudEureka7002Application.java
@@ -0,0 +1,18 @@
+package com.jacklinsir.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author linSir
+ * @version V1.0
+ * @Date 2020/1/28 12:28
+ */
+@SpringBootApplication
+@EnableEurekaServer //启动eureka服务
+public class CloudEureka7002Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEureka7002Application.class, args);
+    }
+}
